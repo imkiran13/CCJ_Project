c9101373f494e4fac42ab083b1bec80a0ef6d07d
kiran <imkiran13@gmail.com>
2024-08-12 12:07:30 +0530

    footer design with responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,23 @@
-$(document).ready(function() {
-  $('#open-sidebar').on('click', function() {
-    $('.header-right').addClass('active');
+$(document).ready(function () {
+  $("#open-sidebar").on("click", function () {
+    $(".header-right").addClass("active");
   });
 
-  $('#close-sidebar').on('click', function() {
-    $('.header-right').removeClass('active');
+  $("#close-sidebar").on("click", function () {
+    $(".header-right").removeClass("active");
   });
 
-  
-   $('.footer_top-box h5').on('click', function() {
-    // Close all other open ULs
-    $('.footer_top-box h5').not(this).removeClass('active').next('ul').slideUp();
+  if (innerWidth <= 991) {
+    $(".footer_top-box h5").on("click", function () {
+      // Close all other open ULs
+      $(".footer_top-box h5")
+        .not(this)
+        .removeClass("active")
+        .next("ul")
+        .slideUp();
 
-    // Toggle the current UL
-    $(this).toggleClass('active').next('ul').slideToggle();
-  });
+      // Toggle the current UL
+      $(this).toggleClass("active").next("ul").slideToggle();
+    });
+  }
 });
